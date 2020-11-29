28a30a6440ceb2505c21ee9d5c01e3176053103b
김재구 <rlaworn1993@gmail.com>
2020-11-29 17:04:16 +0900

    git ignore added

--- src/main/java/com/github/kafkaStudy/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/kafkaStudy/kafka/tutorial2/TwitterProducer.java
@@ -1,23 +1,45 @@
 package com.github.kafkaStudy.kafka.tutorial2;
 
 import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class TwitterProducer {
 
-    public TwitterProducer(){}
+    static String consumerKey;
+    static String consumerSecret;
+    static String token;
+    static String tokenSecret;
+    static TwitterProducer producer;
 
-    public static void main(String[] args) {
-        new TwitterProducer().run();
+    public TwitterProducer(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+        this.consumerKey = consumerKey;
+        this.consumerSecret = consumerSecret;
+        this.token = token;
+        this.tokenSecret = tokenSecret;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        properties.load(TwitterProducer.class.getResourceAsStream("/twitterCredentials.properties"));
+        consumerKey = properties.getProperty("consumerKey");
+        consumerSecret = properties.getProperty("consumerSecret");
+        token = properties.getProperty("token");
+        tokenSecret = properties.getProperty("tokenSecret");
+        producer = new TwitterProducer(consumerKey,consumerSecret,token,tokenSecret);
+        new TwitterProducer(consumerKey,consumerSecret,token,tokenSecret).run();
     }
 
     public void run() {
@@ -28,8 +50,6 @@ public class TwitterProducer {
         // loop to send tweets to kafka
     }
 
-    String consumerKey = "";
-    String cosumer
     public void createTwitterClient() {
         BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
@@ -39,6 +59,17 @@ public class TwitterProducer {
         hosebirdEndpoint.trackTerms(terms);
 
 // These secrets should be read from a config file
-        Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue))
+                .eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
+
+        Client hosebirdClient = builder.build();
+// Attempts to establish a connection.
+        hosebirdClient.connect();
     }
 }
